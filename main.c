8eaaca7129057f2f322645393687ba5d299fa993
calacc <calacc314@gmail.com>
2023-03-30 22:53:12 +0300

    Added the functionalities to the symbolic link menu and cleaned up the code and the messages for both menus

--- main.c
+++ main.c
@@ -8,22 +8,22 @@
 void printMenuRegularFile()
 {
 	printf("You can execute the following operations:\n");
-	printf("-n: file name\n");
-	printf("-d: dim/size\n");
-	printf("-h: number of hard links\n");
-	printf("-m: time of last modification\n");
-	printf("-a: access rights\n");
-	printf("-l: create symbolic link (please give a link name)\n");
+	printf("-n: file name;\n");
+	printf("-d: dim/size;\n");
+	printf("-h: number of hard links;\n");
+	printf("-m: time of last modification;\n");
+	printf("-a: access rights;\n");
+	printf("-l: create symbolic link (please give a link name).\n");
 }
 
 void printMenuSymbolicLink()
 {
 	printf("You can execute the following operations:\n");
-	printf("-n: link name\n");
-	printf("-l: delete link\n");
-	printf("-d: size of the link\n");
-	printf("-t: size of the target\n");
-	printf("-a: access rights\n");
+	printf("-n: link name;\n");
+	printf("-l: delete link;\n");
+	printf("-d: size of the link;\n");
+	printf("-t: size of the target;\n");
+	printf("-a: access rights.\n");
 }
 
 void getName(char *path)
@@ -39,7 +39,7 @@ void executeOperations(char path[])
 	struct stat filestat;
 	if(lstat(path, &filestat)<0)
 	{
-		printf("Eror: unable to stat %s. ", path);
+		printf("Eror: unable to stat \"%s\" ", path);
 		return;
 	}
 	char operations[50];
@@ -59,22 +59,22 @@ void executeOperations(char path[])
 				}
 				case 'd': 
 				{
-					printf("size: %lld\n", (long long)filestat.st_size);
+					printf("Size of \"%s\": %lld\n", path, (long long)filestat.st_size);
 					break;
 				}
 				case 'h':
 				{
-					printf("number of hard links: %d\n", (int)filestat.st_nlink);
+					printf("Number of hard links of \"%s\": %d\n", path, (int)filestat.st_nlink);
 					break;
 				}
 				case 'm': 
 				{
-					printf("time of last modification: %s", ctime(&filestat.st_mtime));
+					printf("Time of last modification of \"%s\": %s", path, ctime(&filestat.st_mtime));
 					break;
 				}
 				case 'a': 
 				{
-					printf("access rights: ");
+					printf("Access rights of %s: ", path);
                     printf((filestat.st_mode & S_IRUSR) ? "r" : "-");
                     printf((filestat.st_mode & S_IWUSR) ? "w" : "-");
                     printf((filestat.st_mode & S_IXUSR) ? "x" : "-");
@@ -90,17 +90,17 @@ void executeOperations(char path[])
 				case 'l': 
 				{
 					char linkname[20];
-					printf("please provide a link name: ");
+					printf("Please provide a link name: ");
 					scanf("%s", linkname);
 					if(symlink(path, linkname)<0)
 					{
-						printf("unable to create symbolic link %s", linkname);
+						printf("Error: unable to create symbolic link \"%s\"", linkname);
 						break;
 					}
-					printf("symbolic link %s was created successfully", linkname);
+					printf("Symbolic link \"%s\" was created successfully", linkname);
 					break;
 				}
-				default: printf("%c is not a valid operation!", operations[i]);
+				default: printf("%c is not a valid operation", operations[i]);
 			}
 		}
 		
@@ -113,19 +113,52 @@ void executeOperations(char path[])
 		
 		for(int i=1; operations[i]; ++i)
 		{
-			// switch(operations[i])
-			// {
-				// case 'n': 
-				// {
-					// getName(path);
-					// break;
-				// }
-				// case 'l': break;
-				// case 'd': break;
-				// case 't': break;
-				// case 'a': break;
-				// default: printf("%c is not a valid operation!", operations[i]);
-			// }
+			switch(operations[i])
+			{
+				case 'n': 
+				{
+					getName(path);
+					break;
+				}
+				case 'l':
+				{
+					if(unlink(path)<0)
+						printf("Deletion of symbolic link \"%s\" was unsuccessful\n", path);
+					else
+						printf("Successfully deleted the symbolik link \"%s\"\n", path);
+					break;
+				}
+				case 'd': 
+				{
+					printf("Size of \"%s\": %lld\n", path, (long long)filestat.st_size);
+					break;
+				}
+				case 't': 
+				{
+					struct stat targetstat;
+					if(stat(path, &targetstat)<0)
+						printf("Error: unable to stat target of \"%s\"\n", path);
+					else
+						printf("Size of target file: %lld\n", (long long) targetstat.st_size);
+					break;
+				}
+				case 'a':
+				{
+					printf("Access rights of \"%s\": ", path);
+                    printf((filestat.st_mode & S_IRUSR) ? "r" : "-");
+                    printf((filestat.st_mode & S_IWUSR) ? "w" : "-");
+                    printf((filestat.st_mode & S_IXUSR) ? "x" : "-");
+                    printf((filestat.st_mode & S_IRGRP) ? "r" : "-");
+                    printf((filestat.st_mode & S_IWGRP) ? "w" : "-");
+                    printf((filestat.st_mode & S_IXGRP) ? "x" : "-");
+                    printf((filestat.st_mode & S_IROTH) ? "r" : "-");
+                    printf((filestat.st_mode & S_IWOTH) ? "w" : "-");
+                    printf((filestat.st_mode & S_IXOTH) ? "x" : "-");
+                    printf("\n");
+					break;
+				}
+				default: printf("%c is not a valid operation!", operations[i]);
+			}
 		}
 		
 		return;
